58381ac7990fdae572be0bab86736bc14fe285f0
Marius Darila <marius.darila@akasha.world>
2023-11-09 17:58:30 +0100

    chore(): increase cache size

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -4,7 +4,7 @@ import { ApolloGateway } from '@apollo/gateway';
 import { readFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache';
+import { InMemoryLRUCache, PrefixingKeyValueCache } from '@apollo/utils.keyvaluecache';
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -16,10 +16,13 @@ const gateway = new ApolloGateway({
   supergraphSdl: supergraph
 });
 
+const cache = new InMemoryLRUCache({
+    maxSize: Math.pow(2, 20) * 128, // ~128 MiB
+  });
 
 const server = new ApolloServer({
   gateway,
-  cache: new InMemoryLRUCache(),
+  cache: new PrefixingKeyValueCache(cache, "gql:"),
   persistedQueries: {
     ttl: 60*60*24, // 24h
   },
