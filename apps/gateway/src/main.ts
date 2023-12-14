52a8cd670bad3312db3fd48ffe8e5166567d7fea
Marius Darila <marius.darila@akasha.world>
2023-12-14 14:40:45 +0100

    chore(cache): add redis backend

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,12 +1,19 @@
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer, PersistedQueryOptions } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { ApolloGateway } from "@apollo/gateway";
 import { readFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { InMemoryLRUCache, PrefixingKeyValueCache } from '@apollo/utils.keyvaluecache';
+import Keyv from "keyv";
+import { KeyvAdapter } from "@apollo/utils.keyvadapter";
+import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl';
+//this is just a reference to the @keyv/redis package, so that the redis package is included in the bundle.
+import _ from "@keyv/redis";
+
 import { ComposeDBDataSource } from "./headers.propagate.js";
 import { VIEWER_ID_HEADER } from '@composedb/constants';
+import * as process from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -21,16 +28,25 @@ const gateway = new ApolloGateway({
   }
 });
 
-const cache = new InMemoryLRUCache({
+let cache: PersistedQueryOptions['cache'];
+if(!!process.env?.REDIS_CONNECTION_STRING?.trim()){
+  cache = new KeyvAdapter(new Keyv(process.env.REDIS_CONNECTION_STRING));
+  console.info("Using Redis cache");
+}else {
+  cache = new InMemoryLRUCache({
     maxSize: Math.pow(2, 20) * 128, // ~128 MiB
   });
+  cache = new PrefixingKeyValueCache(cache, "gql:");
+  console.info("Using In-memory cache");
+}
 
 const server = new ApolloServer({
   gateway,
-  cache: new PrefixingKeyValueCache(cache, "gql:"),
+  cache: cache,
   persistedQueries: {
-    ttl: 60*60*24, // 24h
+    ttl: 60*60*24*2, // 48h
   },
+  plugins: [ApolloServerPluginCacheControl({ defaultMaxAge: 10 })], //10s
 });
 
 
